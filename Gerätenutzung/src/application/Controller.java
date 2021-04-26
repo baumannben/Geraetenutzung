f84202d761ae7dc4975b2ad88b4c402410ce1b42
deben <deben@G14-Ben.speedport.ip>
2021-04-26 09:29:38 +0200

    CSV Einlesen

--- Gerätenutzung/src/application/Controller.java
+++ Gerätenutzung/src/application/Controller.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -9,244 +12,269 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Control;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Circle;
 
-public class Controller  implements Initializable{
-    @FXML
-    private ImageView fahrrad_1;
-    @FXML
-    private ImageView fahrrad_2;
-    @FXML
-    private ImageView fahrrad_3;
-    @FXML
-    private ImageView fahrrad_4;
-    @FXML
-    private ImageView fahrrad_5;
-    @FXML
-    private ImageView fahrrad_6;
-    @FXML
-    private ImageView fahrrad_7;
-    @FXML
-    private ImageView fahrrad_8;
-    @FXML
-    private ImageView hantelset_6;
-    @FXML
-    private ImageView hantelset_2;
-    @FXML
-    private ImageView hantelset_3;
-    @FXML
-    private ImageView hantelset_4;
-    @FXML
-    private ImageView hantelset_1;
-    @FXML
-    private ImageView hantelset_5;
-    @FXML
-    private ImageView beinpresse_1;
-    @FXML
-    private ImageView beinpresse_2;
-    @FXML
-    private ImageView beinpresse_3;
-    @FXML
-    private ImageView beinpresse_4;
-    @FXML
-    private ImageView beinpresse_5;
-    @FXML
-    private ImageView beinpresse_6;
-    @FXML
-    private ImageView hantelbank_1;
-    @FXML
-    private ImageView hantelbank_2;
-    @FXML
-    private ImageView hantelbank_3;
-    @FXML
-    private ImageView hantelbank_4;
-    @FXML
-    private ImageView hantelbank_5;
-    @FXML
-    private ImageView hantelbank_6;
-    @FXML
-    private ImageView latzug_1;
-    @FXML
-    private ImageView latzug_2;
-    @FXML
-    private ImageView latzug_3;
-    @FXML
-    private ImageView latzug_4;
-    
-    public double[] haltbarkeiten = {0.8,0.3,1.0,0.6};
-    
-    public ImageView[] fahrraeder;
-    public ImageView[] latzuege;
-    public ImageView[] hantelbaenke;
-    public ImageView[] beinpressen;
-    public ImageView[] hantelsets;
-    
+public class Controller implements Initializable {
+	@FXML
+	private ImageView fahrrad_1;
+	@FXML
+	private ImageView fahrrad_2;
+	@FXML
+	private ImageView fahrrad_3;
+	@FXML
+	private ImageView fahrrad_4;
+	@FXML
+	private ImageView fahrrad_5;
+	@FXML
+	private ImageView fahrrad_6;
+	@FXML
+	private ImageView fahrrad_7;
+	@FXML
+	private ImageView fahrrad_8;
+	@FXML
+	private ImageView hantelset_6;
+	@FXML
+	private ImageView hantelset_2;
+	@FXML
+	private ImageView hantelset_3;
+	@FXML
+	private ImageView hantelset_4;
+	@FXML
+	private ImageView hantelset_1;
+	@FXML
+	private ImageView hantelset_5;
+	@FXML
+	private ImageView beinpresse_1;
+	@FXML
+	private ImageView beinpresse_2;
+	@FXML
+	private ImageView beinpresse_3;
+	@FXML
+	private ImageView beinpresse_4;
+	@FXML
+	private ImageView beinpresse_5;
+	@FXML
+	private ImageView beinpresse_6;
+	@FXML
+	private ImageView hantelbank_1;
+	@FXML
+	private ImageView hantelbank_2;
+	@FXML
+	private ImageView hantelbank_3;
+	@FXML
+	private ImageView hantelbank_4;
+	@FXML
+	private ImageView hantelbank_5;
+	@FXML
+	private ImageView hantelbank_6;
+	@FXML
+	private ImageView latzug_1;
+	@FXML
+	private ImageView latzug_2;
+	@FXML
+	private ImageView latzug_3;
+	@FXML
+	private ImageView latzug_4;
+
+	public int[] haltbarkeiten = new int[100];
+
+	public ImageView[] fahrraeder;
+	public ImageView[] latzuege;
+	public ImageView[] hantelbaenke;
+	public ImageView[] beinpressen;
+	public ImageView[] hantelsets;
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		fahrraeder  = new ImageView[]{fahrrad_1,fahrrad_2,fahrrad_3,fahrrad_4,fahrrad_5,fahrrad_6,fahrrad_7,fahrrad_8};
-		initFahrraeder();
-		latzuege = new ImageView[] {latzug_1,latzug_2,latzug_3,latzug_4};
-		initLatzuege();
-		hantelbaenke = new ImageView[] {hantelbank_1,hantelbank_2,hantelbank_3,hantelbank_4,hantelbank_5,hantelbank_6};
-		initHantelbaenke();
-		beinpressen = new ImageView[] {beinpresse_1,beinpresse_2,beinpresse_3,beinpresse_4,beinpresse_5,beinpresse_6};
+		// (Controller.class.getResourceAsStream("Geratenutzung.csv"))
+		fahrraeder = new ImageView[] { fahrrad_1, fahrrad_2, fahrrad_3, fahrrad_4, fahrrad_5, fahrrad_6, fahrrad_7,
+				fahrrad_8 };
+		latzuege = new ImageView[] { latzug_1, latzug_2, latzug_3, latzug_4 };
+		hantelbaenke = new ImageView[] { hantelbank_1, hantelbank_2, hantelbank_3, hantelbank_4, hantelbank_5,
+				hantelbank_6 };
+		beinpressen = new ImageView[] { beinpresse_1, beinpresse_2, beinpresse_3, beinpresse_4, beinpresse_5,
+				beinpresse_6 };
+		hantelsets = new ImageView[] { hantelset_1, hantelset_2, hantelset_3, hantelset_4, hantelset_5, hantelset_6 };
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(
+					"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\application\\Geratenutzung.csv"));
+			String line = "";
+			String splitBy = ";";
+			int i = 0;
+			while ((line = br.readLine()) != null) {
+				String[] geraete = line.split(splitBy);
+				System.out.println("ID: " + geraete[0] + " Name: " + geraete[1] + " Datum/Uhrzeit: " + geraete[2]
+						+ " Alter: " + geraete[3] + " Haltbarkeit: " + geraete[4] + " Belastungsgrad: " + geraete[5]
+						+ " Belegt?: " + geraete[6]);
+				if (i >= 1) {
+					haltbarkeiten[i] = Integer.parseInt(geraete[4]);
+				}
+				i++;
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		initAll();
+
+	}
+
+	public void initAll() {
 		initBeinpressen();
-		hantelsets = new ImageView[] {hantelset_1,hantelset_2,hantelset_3,hantelset_4,hantelset_5,hantelset_6};
+		initFahrraeder();
+		initHantelbaenke();
 		initHantelsets();
-		
+		initLatzuege();
 	}
-	
+
 	public void initFahrraeder() {
-		for (int i = 0; i < haltbarkeiten.length; i++) {
-			if (haltbarkeiten[i] < 1 && haltbarkeiten[i] >= 0.7) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
-					fahrraeder[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+		try {
+			int k = 0;
+			for (int i = 1; i < 8; i++) {
+				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
+					fahrraeder[k].setImage(j);
+				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
+					fahrraeder[k].setImage(j);
+
+				} else if (haltbarkeiten[i] <= 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
+					fahrraeder[k].setImage(j);
 				}
+				k++;
 			}
-			else if (haltbarkeiten[i] < 0.7 && haltbarkeiten[i] > 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
-					fahrraeder[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-			else if (haltbarkeiten[i] <= 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
-					fahrraeder[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
+
 	public void initHantelsets() {
-		for (int i = 0; i < haltbarkeiten.length; i++) {
-			if (haltbarkeiten[i] < 1 && haltbarkeiten[i] >= 0.7) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
-					hantelsets[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+		try {
+			int k = 0;
+			for (int i = 19; i < 25; i++) {
+				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-grun.png"));
+					hantelsets[k].setImage(j);
+				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-gelb.png"));
+					hantelsets[k].setImage(j);
+
+				} else if (haltbarkeiten[i] <= 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-rot.png"));
+					hantelsets[k].setImage(j);
 				}
+				k++;
 			}
-			else if (haltbarkeiten[i] < 0.7 && haltbarkeiten[i] > 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
-					hantelsets[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-			else if (haltbarkeiten[i] <= 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
-					hantelsets[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
+
 	public void initLatzuege() {
-		for (int i = 0; i < haltbarkeiten.length; i++) {
-			if (haltbarkeiten[i] < 1 && haltbarkeiten[i] >= 0.7) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
-					latzuege[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+		try {
+			int k = 0;
+			for (int i = 15; i < 19; i++) {
+				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-grun.png"));
+					latzuege[k].setImage(j);
+				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-gelb.png"));
+					latzuege[k].setImage(j);
+
+				} else if (haltbarkeiten[i] <= 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-rot.png"));
+					latzuege[k].setImage(j);
 				}
+				k++;
 			}
-			else if (haltbarkeiten[i] < 0.7 && haltbarkeiten[i] > 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
-					latzuege[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-			else if (haltbarkeiten[i] <= 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
-					latzuege[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
+
 	public void initHantelbaenke() {
-		for (int i = 0; i < haltbarkeiten.length; i++) {
-			if (haltbarkeiten[i] < 1 && haltbarkeiten[i] >= 0.7) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
-					hantelbaenke[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+		try {
+			int k = 0;
+			for (int i = 9; i < 15; i++) {
+				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-grun.png"));
+					hantelbaenke[k].setImage(j);
+				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-gelb.png"));
+					hantelbaenke[k].setImage(j);
+
+				} else if (haltbarkeiten[i] <= 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-rot.png"));
+					hantelbaenke[k].setImage(j);
 				}
+				k++;
 			}
-			else if (haltbarkeiten[i] < 0.7 && haltbarkeiten[i] > 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
-					hantelbaenke[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-			else if (haltbarkeiten[i] <= 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
-					hantelbaenke[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
+
 	public void initBeinpressen() {
-		for (int i = 0; i < haltbarkeiten.length; i++) {
-			if (haltbarkeiten[i] < 1 && haltbarkeiten[i] >= 0.7) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
-					beinpressen[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
+		try {
+			int k = 0;
+			for (int i = 25; i < 31; i++) {
+				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-grun.png"));
+					beinpressen[k].setImage(j);
+				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-gelb.png"));
+					beinpressen[k].setImage(j);
+
+				} else if (haltbarkeiten[i] <= 30) {
+
+					Image j = new Image(new FileInputStream(
+							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-rot.png"));
+					beinpressen[k].setImage(j);
 				}
+				k++;
 			}
-			else if (haltbarkeiten[i] < 0.7 && haltbarkeiten[i] > 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
-					beinpressen[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-			else if (haltbarkeiten[i] <= 0.3) {
-				try {
-					Image j = new Image(new FileInputStream("C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
-					beinpressen[i].setImage(j);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 	}
-    
-    public void openDetails(MouseEvent event) {
-    	String  name = event.getPickResult().getIntersectedNode().getId();
-		Alert  details = new Alert(AlertType.INFORMATION);
+
+	public void openDetails(MouseEvent event) {
+		String name = event.getPickResult().getIntersectedNode().getId();
+		Alert details = new Alert(AlertType.INFORMATION);
 		details.setTitle("Details");
-		details.setHeaderText(name);	
-		details.setContentText("Details:\nHaltbarkeit: "+event.getPickResult().getIntersectedNode().getId());
+		details.setHeaderText(name);
+		details.setContentText("Details:\nHaltbarkeit: " + event.getPickResult().getIntersectedNode().getId());
 		details.show();
-    }
+	}
 }
