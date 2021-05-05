bcb7953f08310914edf2c34321b837ea70831713
deben <deben@G14-Ben>
2021-05-05 10:36:04 +0200

    Datenbank implementiert (Achtung Fehler Vorhanden!)

--- Gerätenutzung/src/application/Controller.java
+++ Gerätenutzung/src/application/Controller.java
@@ -9,8 +9,9 @@ import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.ResourceBundle;
-
+import java.sql.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -93,14 +94,52 @@ public class Controller implements Initializable {
 	public ImageView[] hantelbaenke;
 	public ImageView[] beinpressen;
 	public ImageView[] hantelsets;
+
 	/**
-	 * Hier werden zunächst einmal Image View Gruppen als Arrays
-	 * gebildet welche später zur Zuordnung der Haltbarkeiten wichtig werden.
-	 * Dann wird die CSV Datei mit den jeweiligen Daten ausgelesen und in
-	 * Arrays gespeichert.
+	 * Hier werden zunächst einmal Image View Gruppen als Arrays gebildet welche
+	 * später zur Zuordnung der Haltbarkeiten wichtig werden. Dann wird die CSV
+	 * Datei mit den jeweiligen Daten ausgelesen und in Arrays gespeichert.
 	 */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection("jdbc:oracle:thin:@oracle.s-atiw.de:1521:atiwora", "FS192_bbaumann", "ben");
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT x.*,x.ROWID FROM FS192_LTROESCH.TRAININGSGERAET x");
+			int i = 0;
+			SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+			while (rs.next())
+				System.out.println(rs.getString(1) + "  " + rs.getDate(2)
+				+ "  " + rs.getString(3)+ "  " + rs.getString(4)
+				+ "  " + rs.getString(5)+ "  " + rs.getString(6)
+				+ "  " + rs.getString(7)+ "  " + rs.getString(8)
+				+ "  " + rs.getString(9));
+				try {
+					id[i] = Integer.parseInt(rs.getString(1));
+					date[i] = rs.getDate(2);
+					namen[i] = rs.getString(3);
+					belegt[i] = Boolean.parseBoolean(rs.getString(4));
+					//Lebensdauer 5
+					//Kaufdatum 6
+					haltbarkeiten[i] = Integer.parseInt(rs.getString(7));
+					belastungsgrad[i] = Integer.parseInt(rs.getString(8));
+					//Belegungsplan_ID 9
+				} catch (Exception e) {
+					System.out.println("Fehler in der Datenverarbeitung!");
+					e.printStackTrace();
+				}
+
+			
+			i++;
+			
+			con.close();
+		} catch (Exception e1) {
+			System.out.println("Verbindungsfehler!");
+			e1.printStackTrace();
+		}
+		
 		fahrraeder = new ImageView[] { fahrrad_1, fahrrad_2, fahrrad_3, fahrrad_4, fahrrad_5, fahrrad_6, fahrrad_7,
 				fahrrad_8 };
 		latzuege = new ImageView[] { latzug_1, latzug_2, latzug_3, latzug_4 };
@@ -109,7 +148,7 @@ public class Controller implements Initializable {
 		beinpressen = new ImageView[] { beinpresse_1, beinpresse_2, beinpresse_3, beinpresse_4, beinpresse_5,
 				beinpresse_6 };
 		hantelsets = new ImageView[] { hantelset_1, hantelset_2, hantelset_3, hantelset_4, hantelset_5, hantelset_6 };
-		try {
+		/*try {
 			BufferedReader br = new BufferedReader(new FileReader(
 					"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\application\\Geratenutzung.csv"));
 			String line = "";
@@ -138,9 +177,10 @@ public class Controller implements Initializable {
 			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
+		}*/
 		initAll();
 	}
+
 	/**
 	 * Initialisiert alle Image-Views mit den zu ihrer Haltbarkeit passenden Images
 	 */
@@ -151,28 +191,35 @@ public class Controller implements Initializable {
 		initHantelsets();
 		initLatzuege();
 	}
+
 	public void initFahrraeder() {
 		try {
 			int k = 0;
-			for (int i = 1; i < 8; i++) {
-				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+			int l = 0;
+			for (String s : namen) {
+				if (l >= 1) {
+					if (s.contains("Fahrrad")) {
+						if (haltbarkeiten[l] < 100 && haltbarkeiten[l] >= 70) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
-					fahrraeder[k].setImage(j);
-				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-grun.png"));
+							fahrraeder[k].setImage(j);
+						} else if (haltbarkeiten[l] < 70 && haltbarkeiten[l] > 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
-					fahrraeder[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-gelb.png"));
+							fahrraeder[k].setImage(j);
 
-				} else if (haltbarkeiten[i] <= 30) {
+						} else if (haltbarkeiten[l] <= 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
-					fahrraeder[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Bike-rot.png"));
+							fahrraeder[k].setImage(j);
+						}
+						k++;
+					}
 				}
-				k++;
+				l++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -182,25 +229,31 @@ public class Controller implements Initializable {
 	public void initHantelsets() {
 		try {
 			int k = 0;
-			for (int i = 19; i < 25; i++) {
-				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+			int l = 0;
+			for (String s : namen) {
+				if (l >= 1) {
+					if (s.contains("Hantelset")) {
+						if (haltbarkeiten[l] < 100 && haltbarkeiten[l] >= 70) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-grun.png"));
-					hantelsets[k].setImage(j);
-				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-grun.png"));
+							hantelsets[k].setImage(j);
+						} else if (haltbarkeiten[l] < 70 && haltbarkeiten[l] > 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-gelb.png"));
-					hantelsets[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-gelb.png"));
+							hantelsets[k].setImage(j);
 
-				} else if (haltbarkeiten[i] <= 30) {
+						} else if (haltbarkeiten[l] <= 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-rot.png"));
-					hantelsets[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Dumbell-rot.png"));
+							hantelsets[k].setImage(j);
+						}
+						k++;
+					}
 				}
-				k++;
+				l++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -210,25 +263,31 @@ public class Controller implements Initializable {
 	public void initLatzuege() {
 		try {
 			int k = 0;
-			for (int i = 15; i < 19; i++) {
-				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+			int l = 0;
+			for (String s : namen) {
+				if (l >= 1) {
+					if (s.contains("Latzug")) {
+						if (haltbarkeiten[l] < 100 && haltbarkeiten[l] >= 70) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-grun.png"));
-					latzuege[k].setImage(j);
-				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-grun.png"));
+							latzuege[k].setImage(j);
+						} else if (haltbarkeiten[l] < 70 && haltbarkeiten[l] > 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-gelb.png"));
-					latzuege[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-gelb.png"));
+							latzuege[k].setImage(j);
 
-				} else if (haltbarkeiten[i] <= 30) {
+						} else if (haltbarkeiten[l] <= 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-rot.png"));
-					latzuege[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Latzug-rot.png"));
+							latzuege[k].setImage(j);
+						}
+						k++;
+					}
 				}
-				k++;
+				l++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -238,25 +297,31 @@ public class Controller implements Initializable {
 	public void initHantelbaenke() {
 		try {
 			int k = 0;
-			for (int i = 9; i < 15; i++) {
-				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+			int l = 0;
+			for (String s : namen) {
+				if (l >= 1) {
+					if (s.contains("Hantelbank")) {
+						if (haltbarkeiten[l] < 100 && haltbarkeiten[l] >= 70) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-grun.png"));
-					hantelbaenke[k].setImage(j);
-				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-grun.png"));
+							hantelbaenke[k].setImage(j);
+						} else if (haltbarkeiten[l] < 70 && haltbarkeiten[l] > 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-gelb.png"));
-					hantelbaenke[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-gelb.png"));
+							hantelbaenke[k].setImage(j);
 
-				} else if (haltbarkeiten[i] <= 30) {
+						} else if (haltbarkeiten[l] <= 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-rot.png"));
-					hantelbaenke[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Hantelbank-rot.png"));
+							hantelbaenke[k].setImage(j);
+						}
+						k++;
+					}
 				}
-				k++;
+				l++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -266,32 +331,40 @@ public class Controller implements Initializable {
 	public void initBeinpressen() {
 		try {
 			int k = 0;
-			for (int i = 25; i < 31; i++) {
-				if (haltbarkeiten[i] < 100 && haltbarkeiten[i] >= 70) {
+			int l = 0;
+			for (String s : namen) {
+				
+					if (s.contains("Beinpresse")) {
+						if (haltbarkeiten[l] < 100 && haltbarkeiten[l] >= 70) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-grun.png"));
-					beinpressen[k].setImage(j);
-				} else if (haltbarkeiten[i] < 70 && haltbarkeiten[i] > 30) {
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-grun.png"));
+							beinpressen[k].setImage(j);
+						} else if (haltbarkeiten[l] < 70 && haltbarkeiten[l] > 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-gelb.png"));
-					beinpressen[k].setImage(j);
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-gelb.png"));
+							beinpressen[k].setImage(j);
 
-				} else if (haltbarkeiten[i] <= 30) {
+						} else if (haltbarkeiten[l] <= 30) {
 
-					Image j = new Image(new FileInputStream(
-							"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-rot.png"));
-					beinpressen[k].setImage(j);
-				}
-				k++;
+							Image j = new Image(new FileInputStream(
+									"C:\\Users\\deben\\git\\Geraetenutzung\\Gerätenutzung\\src\\images\\Legpress-rot.png"));
+							beinpressen[k].setImage(j);
+						}
+						k++;
+					}
+				
+				l++;
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * Hier werden die Daten der Geräte ausgegeben
+	 * 
 	 * @param event MouseClick auf das jeweilige Gerät
 	 */
 	public void openDetails(MouseEvent event) {
